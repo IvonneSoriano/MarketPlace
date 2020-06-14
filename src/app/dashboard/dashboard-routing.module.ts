bd9fdbbda3275ff8b08ca1e1e2fc6fb0fd543151
IvonneSoriano <ivonnesoriano25@gmail.com>
2020-06-13 20:06:33 -0600

    added the navbar

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }, { path: 'seller', loadChildren: () => import('./seller/seller.module').then(m => m.SellerModule) }];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+   { path: 'seller', loadChildren: () => import('./seller/seller.module').then(m => m.SellerModule) }, 
+   { path: 'client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
