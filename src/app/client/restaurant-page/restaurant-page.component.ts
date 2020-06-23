0844d7a78cc94570d447376770d6a1d18cbface4
IvonneSoriano <ivonnesoriano25@gmail.com>
2020-06-22 21:26:00 -0600

    added the promo

--- src/app/client/restaurant-page/restaurant-page.component.ts
+++ src/app/client/restaurant-page/restaurant-page.component.ts
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantPageComponent implements OnInit {
 
+  //ATRIBUTOS
+  //Este es el resultado
+  public result = { 
+    "shopId":1,
+    "menuId":40,
+    "name":"menu del lunes",
+    "active":1,
+    "period":1,
+    "date":"",
+    "menuDetails":[
+      {
+        productName:"pupusa de frijol",
+        unitaryPrice:0.25,
+        productCategory:""
+      },
+      {
+        productName:"pupusa de queso",
+        unitaryPrice:0.35,
+        productCategory:""
+      }
+    
+    ],
+    "promotions":[
+      {
+        name:"promoción rikisima",
+        "price":4.5,
+        promotionDetails:[
+          {
+            "nameProduct":"Pupusa de frijol con queso",
+            "quantity":10
+          },
+          {
+            "nameProduct":"Coca cola 1L",
+            "quantity":1
+          }
+        ]	
+      }
+    
+    ]
+    };    
+  public cantProducto = 1;
+  public menus =[{
+
+  }];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  //Metodo para sumar Cantidad de producto
+  plussProduct(){
+    this.cantProducto++;
+  }
+  //Metodo para sumar Cantidad de producto
+  lessProduct(){
+    if(this.cantProducto >1){
+      this.cantProducto--;
+    }
+    
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,16 @@ import { NavComponent } from './nav/nav.component';
 import { SearchingBarComponent } from './searching-bar/searching-bar.component';
 import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
 import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
+import { PromoComponent } from './promo/promo.component';
 
 
 
 @NgModule({
-  declarations: [NavComponent, SearchingBarComponent, RestaurantCardComponent, ProductComponent],
+  declarations: [NavComponent, SearchingBarComponent, RestaurantCardComponent, ProductComponent, FooterComponent, PromoComponent],
   imports: [
     CommonModule
   ],
-  exports: [NavComponent, SearchingBarComponent, RestaurantCardComponent, ProductComponent]
+  exports:[NavComponent, SearchingBarComponent, RestaurantCardComponent, ProductComponent, FooterComponent, PromoComponent]
 })
 export class SharedModule { }
